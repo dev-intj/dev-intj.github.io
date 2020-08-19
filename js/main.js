851932ac018bc45d6baea9c3f2ed28ccf3c873e8
Marvelosian <42313770+Marvelosian@users.noreply.github.com>
2020-08-19 23:08:32 +0300

    push update

--- js/main.js
+++ js/main.js
@@ -26,3 +26,55 @@ var ready = () => {
 
 $(document).ready(ready);
 $(document).on("page:load", ready);
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDr_8q2Wuzbp3h4MzB_MGOc2YCkbpKxMJw",
+  authDomain: "personalwebsite-6935f.firebaseapp.com",
+  databaseURL: "https://personalwebsite-6935f.firebaseio.com",
+  projectId: "personalwebsite-6935f",
+  storageBucket: "personalwebsite-6935f.appspot.com",
+  messagingSenderId: "330896932366",
+  appId: "1:330896932366:web:d5fe9e3445c6287b3dc65f",
+  measurementId: "G-TSZPKPS981"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+//Listen for form Submit
+document.getElementById('contactform').addEventListener('submit',submitForm);
+function submitForm(e){
+
+  e.preventDefault();
+  //get values
+
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var details = getInputVal('details');
+
+  //save message
+  saveMessage(name,email,details);
+
+}
+
+
+// FUNCTIONS
+
+// function to get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// save message to firebasejs
+function saveMessage(name,email,details){
+  var newMesssageRef = messagesRef.push();
+  newMesssageRef.set({
+    name: name,
+    email: email,
+    details: details
+  });
+}
