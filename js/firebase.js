ff985398f4a92c659fc464add8ab4266ec8649c0
Marvelosian <42313770+Marvelosian@users.noreply.github.com>
2020-09-22 23:39:51 +0300

    push fix

--- js/firebase.js
+++ js/firebase.js
@@ -1,32 +1,3 @@
-var ready = () => {
-
-    $('.tab').on('click', (e) => {
-        var tabName = (e.currentTarget.attributes[0].nodeValue);
-        removeActive();
-      hideAll();
-      console.log(tabName)
-      $('#' + tabName).addClass('is-active');
-      $('#' + tabName + '-content').removeClass('is-hidden');
-    });
-
-    var removeActive = () => {
-      $('li').each(function() {
-        $(this).removeClass('is-active');
-      });
-    }
-
-    var hideAll = () => {
-      $('#webdesign-webdevelopment-tab-content').addClass('is-hidden');
-      $('#graphicdesign-tab-content').addClass('is-hidden');
-      $('#gamedevelopment-tab-content').addClass('is-hidden');
-      $('#daily-tab-content').addClass('is-hidden');
-    }
-
-}
-
-$(document).ready(ready);
-$(document).on("page:load", ready);
-
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyDr_8q2Wuzbp3h4MzB_MGOc2YCkbpKxMJw",
