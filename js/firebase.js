e972c45db9d6fcf13e1c57118fd2b816d55e3c50
marvelosian <demataschristiano@gmail.com>
2021-03-14 15:41:15 +0200

    changed firebase rules to only accept from this domain

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,13 @@
-import { firebaseConfig } from '/firebaseconfig.js'
+var firebaseConfig = {
+  apiKey: "AIzaSyDr_8q2Wuzbp3h4MzB_MGOc2YCkbpKxMJw",
+  authDomain: "personalwebsite-6935f.firebaseapp.com",
+  databaseURL: "https://personalwebsite-6935f.firebaseio.com",
+  projectId: "personalwebsite-6935f",
+  storageBucket: "personalwebsite-6935f.appspot.com",
+  messagingSenderId: "330896932366",
+  appId: "1:330896932366:web:cf74772a7918c8b33dc65f",
+  measurementId: "G-STZCYG8TV7"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
@@ -7,7 +16,7 @@ var messagesRef = firebase.database().ref('messages');
 
 //Listen for form Submit
 
-function submitForm(e){
+function submitForm(e) {
 
   e.preventDefault();
   //get values
@@ -19,32 +28,32 @@ function submitForm(e){
   //var position = getInputVal('position');
 
   //save message
-  saveMessage(name,email,details);
-//saveMessage(name,email,details,type,position);
+  saveMessage(name, email, details);
+  //saveMessage(name,email,details,type,position);
 
   //show response
   console.log('Your message has been sent.');
   $('#alert').removeClass('is-hidden');
 
-  setTimeout(function(){
-  $('#alert').addClass('is-hidden');
-  },3000);
+  setTimeout(function () {
+    $('#alert').addClass('is-hidden');
+  }, 3000);
 
   //clear form
   document.getElementById('contactform').reset();
 }
-document.getElementById('contactform').addEventListener('submit',submitForm);
+document.getElementById('contactform').addEventListener('submit', submitForm);
 
 
 // FUNCTIONS
 
 // function to get form values
-function getInputVal(id){
+function getInputVal(id) {
   return document.getElementById(id).value;
 }
 
 // save message to firebasejs
-function saveMessage(name,email,details){
+function saveMessage(name, email, details) {
   var newMesssageRef = messagesRef.push();
   newMesssageRef.set({
     name: name,
