cb45d793d6d4520f5fb590a2ef96b40421f1e905
marvelosian <demataschristiano@gmail.com>
2021-03-26 01:19:45 +0200

    config firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,15 @@ import 'firebase/firebase-analytics';
 import 'firebase/firebase-database';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDP78cZKfh1T9Y6l3fN3zmawc1BJLG1RwY",
-    authDomain: "test-493f2.firebaseapp.com",
-    databaseURL: "https://test-493f2-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "test-493f2",
-    storageBucket: "test-493f2.appspot.com",
-    messagingSenderId: "604898703573",
-    appId: "1:604898703573:web:f3ba83bad0af14bd1cca5c"
-};
+    apiKey: "AIzaSyDr_8q2Wuzbp3h4MzB_MGOc2YCkbpKxMJw",
+    authDomain: "personalwebsite-6935f.firebaseapp.com",
+    databaseURL: "https://personalwebsite-6935f.firebaseio.com",
+    projectId: "personalwebsite-6935f",
+    storageBucket: "personalwebsite-6935f.appspot.com",
+    messagingSenderId: "330896932366",
+    appId: "1:330896932366:web:cf74772a7918c8b33dc65f",
+    measurementId: "G-STZCYG8TV7"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
